230043fad761d1015965dc84143d4314d0734d40
you@example.com <you@example.com>
2020-03-06 10:22:13 +0530

    styles

--- MyApp/src/app/app.component.ts
+++ MyApp/src/app/app.component.ts
@@ -5,38 +5,59 @@ import { Component } from "@angular/core";
   templateUrl: "./app.component.html",
   styles: [
     `
-      ul {
-        border: solid red;
+      div {
+        width: 300px;
+        height: 200px;
+        border: solid white;
         border-radius: 5px;
-        box-shadow: 1px 2px 40px orange;
+        box-shadow: 10px 22px 22px gray;
         display: inline-block;
         padding: 5px;
         margin: 5px;
+      }
+      h1 {
+        color: #efff009e;
+        background-color: gray;
+        text-align: center;
+      }
+      h2 {
+        color: aquamarine;
       }
     `
   ]
 })
 export class AppComponent {
-  title = "hello";
-  names = ["rekha", "hdjshd", "akels"];
+  title = "Mobile Shopee";
+  names = ["Nokia", "Samsung", "MicroMax", "Apple"];
 
   mobObjects = [
     {
       id: 1001,
+
       name: "Nokia",
       price: 12500,
+      desc: "Good battery backup,durable",
+      Available: 10
+    },
+    {
+      id: 1002,
+      name: "Samsung",
+      price: 15200,
+      desc: "Best Model designing, Battery backup",
       Available: 0
     },
     {
-      id: 1001,
-      name: "Nokia",
-      price: 12500,
-      Available: 0
+      id: 1003,
+      name: "Apple",
+      price: 52000,
+      desc: "Best model, battery backup",
+      Available: 20
     },
     {
-      id: 1001,
-      name: "Nokia",
-      price: 12500,
+      id: 1004,
+      name: "MacroMax",
+      price: 7000,
+      desc: "Best model with good battery backup",
       Available: 0
     }
   ];
